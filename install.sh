7842b29557d3daeb9200d6828065af614e1d7184
declancm <declanmullen3.14@gmail.com>
2021-11-23 13:02:01 -0800

    Updated install.sh to give errors when vscode is not installed.

--- install.sh
+++ install.sh
@@ -3,15 +3,23 @@ stow nvim
 stow tmux
 
 #echo "#include \"keybindings/keybindings.json\" " >> $HOME/.config/Code/User/keybindings.json
-if [ -f $HOME/.config/Code/User/keybindings.json ] && [ ! -L $HOME/.config/Code/User/keybindings.json ]
+if [ ! -f $HOME/.config/Code/User/settings.json ]
 then
-    mkdir $HOME/.config/Code/User/backup
-    mv -f $HOME/.config/Code/User/keybindings.json $HOME/.config/Code/User/backup/keybindings.json
-fi
+    echo "vscode is not installed."
+else
+    if [ -f $HOME/.config/Code/User/keybindings.json ] && [ ! -L $HOME/.config/Code/User/keybindings.json ]
+    then
+        mkdir $HOME/.config/Code/User/backup
+        mv -f $HOME/.config/Code/User/keybindings.json $HOME/.config/Code/User/backup/keybindings.json
+        echo "A keybindings.json file already exists and was moved to: $HOME/.config/Code/User/backup/keybindings.json"
+    fi
 
-if [ ! -f $HOME/.config/Code/User/keybindings.json ] || [ ! -L $HOME/.config/Code/User/keybindings.json ]
-then
-    stow vscode
+    if [ ! -L $HOME/.config/Code/User/keybindings.json ]
+    then
+        stow vscode
+    else
+        echo "A keybindings.json symlink already exists."
+    fi
 fi
 
 stow x
