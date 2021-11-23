63cbe7843cb8a6e48f561008b9aabefa71804a5f
declancm <declanmullen3.14@gmail.com>
2021-11-22 17:30:42 -0800

    Restructuring.

--- config/.config/nvim/plug-config/lspconfig.lua
+++
@@ -1,7 +0,0 @@
-require'lspconfig'.pyright.setup{}
-require'lspconfig'.clangd.setup{}
-require'lspconfig'.vimls.setup{}
-
--- TO USE clangd FOR A C++ PROJECT, THE FOLLOWING NEEDS TO BE ADDED TO THE
--- CMakeLists.txt:
--- set(CMAKE_EXPORT_COMPILE_COMMANDS ON CACHE INTERNAL "")
