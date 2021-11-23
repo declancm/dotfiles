b989a9beed43c62bf6668f1957632964c6d9c4bf
declancm <declanmullen3.14@gmail.com>
2021-11-23 12:12:13 -0800

    Added uninstall file.

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ stow tmux
 #echo "#include \"keybindings/keybindings.json\" " >> $HOME/.config/Code/User/keybindings.json
 if [ -f $HOME/.config/Code/User/keybindings.json ] && [ ! -L $HOME/.config/Code/User/keybindings.json ]
 then
-    mkdir $HOME/.config/Code/User/backupKeybindings
-    mv -f $HOME/.config/Code/User/keybindings.json $HOME/.config/Code/User/backupKeybindings/keybindings.json
+    mkdir $HOME/.config/Code/User/backup
+    mv -f $HOME/.config/Code/User/keybindings.json $HOME/.config/Code/User/backup/keybindings.json
 fi
 
 if [ ! -f $HOME/.config/Code/User/keybindings.json ] || [ ! -L $HOME/.config/Code/User/keybindings.json ]
---
+++ uninstall.sh
@@ -0,0 +1,6 @@
+stow -D bash
+stow -D nvim
+stow -D tmux
+stow -D vscode
+stow -D x
+stow -D zsh
