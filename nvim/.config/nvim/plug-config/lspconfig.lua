5bf2aec8891ea309c56f2d40ddcf96a278f7a6fc
declancm <declanmullen3.14@gmail.com>
2021-11-23 16:53:34 -0800

    Added custom completion icons to lspconfig.

--- nvim/.config/nvim/plug-config/lspconfig.lua
+++ nvim/.config/nvim/plug-config/lspconfig.lua
@@ -5,3 +5,33 @@ require'lspconfig'.vimls.setup{}
 -- TO USE clangd FOR A C++ PROJECT, THE FOLLOWING NEEDS TO BE ADDED TO THE
 -- CMakeLists.txt:
 -- set(CMAKE_EXPORT_COMPILE_COMMANDS ON CACHE INTERNAL "")
+
+local on_attach = function(client, bufnr)
+  protocol.CompletionItemKind = {
+    '', -- Text
+    '', -- Method
+    '', -- Function
+    '', -- Constructor
+    '', -- Field
+    '', -- Variable
+    '', -- Class
+    'ﰮ', -- Interface
+    '', -- Module
+    '', -- Property
+    '', -- Unit
+    '', -- Value
+    '', -- Enum
+    '', -- Keyword
+    '﬌', -- Snippet
+    '', -- Color
+    '', -- File
+    '', -- Reference
+    '', -- Folder
+    '', -- EnumMember
+    '', -- Constant
+    '', -- Struct
+    '', -- Event
+    'ﬦ', -- Operator
+    '', -- TypeParameter
+  }
+end
