c563fe4ce3944d894d650549b679b96f447e8bc4
declancm <declanmullen3.14@gmail.com>
2021-11-23 22:56:03 -0800

    Switched from fzy to fzf for telescope.

--- nvim/.config/nvim/init.vim
+++ nvim/.config/nvim/init.vim
@@ -28,7 +28,8 @@ Plug 'tami5/lspsaga.nvim'
 Plug 'nvim-lua/popup.nvim'
 Plug 'nvim-lua/plenary.nvim'
 Plug 'nvim-telescope/telescope.nvim'
-Plug 'nvim-telescope/telescope-fzy-native.nvim'
+Plug 'nvim-telescope/telescope-fzf-native.nvim', { 'do': 'make' }
+"Plug 'nvim-telescope/telescope-fzy-native.nvim'
 " lsp
 Plug 'neovim/nvim-lspconfig'
 Plug 'hrsh7th/nvim-compe'
--- nvim/.config/nvim/plug-config/telescope.lua
+++ nvim/.config/nvim/plug-config/telescope.lua
@@ -26,10 +26,20 @@ require('telescope').setup{
     --   extension_config_key = value,
     -- }
     -- please take a look at the readme of the extension you want to configure
-    fzy_native = {
-            override_generic_sorter = false,
-            override_file_sorter = true,
-        }
+
+    --fzy_native = {
+    --        override_generic_sorter = false,
+    --        override_file_sorter = true,
+    --    }
+
+    fzf = {
+      fuzzy = true,                    -- false will only do exact matching
+      override_generic_sorter = true,  -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                       -- the default case_mode is "smart_case"
+    }
   }
 }
-require('telescope').load_extension('fzy_native')
+--require('telescope').load_extension('fzy_native')
+require('telescope').load_extension('fzf')
