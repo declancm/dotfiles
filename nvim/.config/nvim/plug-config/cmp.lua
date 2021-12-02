19ad7f3bce9613f1bab6258725f95e9901a3499f
declancm <declanmullen3.14@gmail.com>
2021-12-02 01:51:10 -0800

    Minor update.

--- nvim/.config/nvim/plug-config/cmp.lua
+++ nvim/.config/nvim/plug-config/cmp.lua
@@ -5,9 +5,9 @@
     snippet = {
       -- REQUIRED - you must specify a snippet engine
       expand = function(args)
-        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
         -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+        vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
         -- require'snippy'.expand_snippet(args.body) -- For `snippy` users.
       end,
     },
@@ -24,9 +24,9 @@
     },
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
-      { name = 'vsnip' }, -- For vsnip users.
+      -- { name = 'vsnip' }, -- For vsnip users.
       -- { name = 'luasnip' }, -- For luasnip users.
-      -- { name = 'ultisnips' }, -- For ultisnips users.
+      { name = 'ultisnips' }, -- For ultisnips users.
       -- { name = 'snippy' }, -- For snippy users.
     }, {
       { name = 'buffer' },
@@ -58,6 +58,6 @@ local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protoco
   require('lspconfig')['clangd'].setup {
     capabilities = capabilities
   }
-  require('lspconfig')['vimls'].setup {
-    capabilities = capabilities
-  }
+  -- require('lspconfig')['vimls'].setup {
+  --   capabilities = capabilities
+  -- }
