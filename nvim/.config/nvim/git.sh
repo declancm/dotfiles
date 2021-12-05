592753b8ba47a1c260ecc1749b7d35a46414bcc9
declancm <declanmullen3.14@gmail.com>
2021-12-04 21:57:35 -0800

    auto commit @ 2021-12-04 21:57:35

--- nvim/.config/nvim/git.sh
+++ nvim/.config/nvim/git.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-branch=$(git rev-parse --abbrev-ref HEAD)
-gitDirectory=$(git rev-parse --show-toplevel)
+if git rev-parse --git-dir > /dev/null 2>&1; then
+    branch=$(git rev-parse --abbrev-ref HEAD)
+    gitDirectory=$(git rev-parse --show-toplevel)
 
-cd $gitDirectory
-git add .
-wait
-git commit -a -m "auto commit @ `date +'%Y-%m-%d %H:%M:%S'`"
-wait
-git push origin $branch
+    cd $gitDirectory
+    git add .
+    wait
+    git commit -a -m "auto commit @ `date +'%Y-%m-%d %H:%M:%S'`"
+    wait
+    git push origin $branch
+else
+    printf "Not inside a git repository."
+fi
