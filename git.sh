78aaa1024ede61ececdf30618c8982f73146d268
declancm <declanmullen3.14@gmail.com>
2021-12-04 21:48:40 -0800

    auto commit @ 2021-12-04 21:48:40

--- git.sh
+++ git.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 branch=$(git rev-parse --abbrev-ref HEAD)
+gitDirectory=$(git rev-parse --show-toplevel)
 
+cd $gitDirectory
 git add .
 wait
 git commit -a -m "auto commit @ `date +'%Y-%m-%d %H:%M:%S'`"
---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+directory=$(pwd)
+printf "$directory"
