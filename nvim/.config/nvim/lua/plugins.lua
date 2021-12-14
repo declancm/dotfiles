f0b34de961c5a495bbb84270c1ee5b3f38f79881
declancm <declanmullen3.14@gmail.com>
2021-12-13 19:36:59 -0800

    auto commit @ 2021-12-13 19:36:59

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -14,21 +14,21 @@ return require('packer').startup(function()
   -- " snippets
 
   -- cmp completion
-  -- use {
-  --   'saadparwaiz1/cmp_luasnip',
-  --   requires = {'L3MON4D3/LuaSnip', 'rafamadriz/friendly-snippets'}
-  -- }
-  -- use 'hrsh7th/cmp-nvim-lsp'
-  -- use 'hrsh7th/cmp-buffer'
-  -- use 'hrsh7th/cmp-path'
-  -- use 'hrsh7th/cmp-cmdline'
-  -- use 'hrsh7th/nvim-cmp'
-  -- use 'onsails/lspkind-nvim'
-  -- use {'tzachar/cmp-tabnine', run = './install.sh'}
+  use {
+    'saadparwaiz1/cmp_luasnip',
+    requires = {'L3MON4D3/LuaSnip', 'rafamadriz/friendly-snippets'}
+  }
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
+  use 'onsails/lspkind-nvim'
+  use {'tzachar/cmp-tabnine', run = './install.sh'}
 
   -- coc completion
-  use {'neoclide/coc.nvim', run = 'yarn install --frozen-lockfile'}
-  -- use 'wellle/tmux-complete.vim'
+  -- use {'neoclide/coc.nvim', run = 'yarn install --frozen-lockfile'}
+  -- -- use 'wellle/tmux-complete.vim'
 
   -- commenting
   use 'b3nj5m1n/kommentary'
