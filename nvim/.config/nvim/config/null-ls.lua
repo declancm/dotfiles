dddeb3b3be0697abd5d738ee13d3c3648758094a
declancm <declanmullen3.14@gmail.com>
2022-02-04 23:05:35 +1100

    auto commit @ 2022-02-04 12:05:35 UTC

--- nvim/.config/nvim/config/null-ls.lua
+++ nvim/.config/nvim/config/null-ls.lua
@@ -1,27 +1,28 @@
 require("null-ls").setup({
-	debug = false,
-	sources = {
-		require("null-ls").builtins.formatting.black,
-		-- pip3 install black
-		require("null-ls").builtins.formatting.clang_format,
-		-- sudo apt install clang-format
-		require("null-ls").builtins.formatting.prettier,
-		-- npm install --save-dev --save-exact prettier
-		-- require("null-ls").builtins.formatting.stylua,
-		-- cargo install stylua
-	},
-	-- automatically format on save
-	on_attach = function(client)
-		if client.resolved_capabilities.document_formatting then
-			vim.cmd([[
+  debug = false,
+  sources = {
+    require("null-ls").builtins.formatting.black,
+    -- pip3 install black
+    require("null-ls").builtins.formatting.clang_format,
+    -- sudo apt install clang-format
+    require("null-ls").builtins.formatting.prettier,
+    -- npm install --save-dev --save-exact prettier
+    require("null-ls").builtins.formatting.stylua,
+    -- cargo install stylua
+  },
+  -- automatically format on save
+  on_attach = function(client)
+    if client.resolved_capabilities.document_formatting then
+      vim.cmd([[
             augroup LspFormatting
                 autocmd! * <buffer>
                 autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
-                -- autocmd BufWritePre <buffer> retab
+                " convert tabs to spaces
+                autocmd BufWritePre <buffer> retab
             augroup END
             ]])
-		end
-	end,
+    end
+  end,
 })
 
 -- format on command
