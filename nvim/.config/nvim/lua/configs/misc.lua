3641c91dd4553ae829bdd6189d81f3cc51472cec
declancm <declanmullen3.14@gmail.com>
2022-03-02 19:21:25 +1100

    Auto Commit: Mar 02 08:21:25 2022 UTC

--- nvim/.config/nvim/lua/configs/misc.lua
+++ nvim/.config/nvim/lua/configs/misc.lua
@@ -1,6 +1,5 @@
 local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
-local buf_keymap = vim.api.nvim_buf_set_keymap
 
 -- UNDOTREE:
 
@@ -21,9 +20,5 @@ keymap('n', '<leader>cq', '<Cmd>call setqflist([])<CR>', opts)
 
 -- MARKDOWN-PREVIEW:
 
-vim.cmd 'autocmd FileType markdown lua MarkdownPreview()'
-
-function MarkdownPreview()
-  buf_keymap(0, 'n', '<leader>md', '<Cmd>MarkdownPreview<CR>', opts)
-  buf_keymap(0, 'n', '<leader>ms', '<Cmd>MarkdownPreviewStop<CR>', opts)
-end
+keymap('n', '<leader>md', '<Cmd>MarkdownPreview<CR>', opts)
+keymap('n', '<leader>ms', '<Cmd>MarkdownPreviewStop<CR>', opts)
