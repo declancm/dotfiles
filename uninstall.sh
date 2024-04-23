978c0adb343477df9ccfb97f89cac7f98f714d57
Declan Mullen <declanmullen3.14@gmail.com>
2024-04-22 17:33:10 -0700

    Updated uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -1,20 +1,5 @@
 stow -D alacritty
 stow -D bash
-if [ -L $HOME/.local/share/fonts/'Hack Regular Nerd Font Complete.ttf' ]
-then
-    stow -D fonts
-fi
+stow -D fonts
 stow -D nvim
 stow -D tmux
-if [ -L $HOME/.config/Code/User/keybindings.json ]
-then
-    stow -D vscode
-    if [ -f $HOME/.config/Code/User/backup/keybindings.json ]
-    then
-        mv -f $HOME/.config/Code/User/backup/keybindings.json $HOME/.config/Code/User/keybindings.json
-        rmdir $HOME/.config/Code/User/backup
-    fi
-fi
-stow -D x
-stow -D zsh
-rm -rf ~/.tmux
