2168333912de76bf7788756677be0fd106381b36
Declan Mullen <declanmullen3.14@gmail.com>
2024-05-04 14:01:12 -0700

    Wrap text movement keymaps with lazyredraw

--- nvim/dot-config/nvim/lua/keymaps.lua
+++ nvim/dot-config/nvim/lua/keymaps.lua
@@ -1,14 +1,18 @@
+local lazyredraw = function(command)
+  return '<cmd>set lz<cr>' .. command .. '<cmd>set nolz<cr>'
+end
+
 -- Scrolling.
-vim.keymap.set({ 'n', 'x' }, '<c-u>', '<cmd>set lz<cr><c-u>zz<cmd>set nolz | redraw<cr>', {})
-vim.keymap.set({ 'n', 'x' }, '<c-d>', '<cmd>set lz<cr><c-d>zz<cmd>set nolz | redraw<cr>', {})
+vim.keymap.set({ 'n', 'x' }, '<c-u>', lazyredraw('<c-u>zz'), {})
+vim.keymap.set({ 'n', 'x' }, '<c-d>', lazyredraw('<c-d>zz'), {})
 
 -- Moving text.
-vim.keymap.set('x', '<c-up>', ":m '<-2<cr>gv=gv", {})
-vim.keymap.set('x', '<c-down>', ":m '>+1<cr>gv=gv", {})
-vim.keymap.set('i', '<c-up>', '<esc><cmd>m .-2<cr>==a', {})
-vim.keymap.set('i', '<c-down>', '<esc><cmd>m .+1<cr>==a', {})
-vim.keymap.set('n', '<c-up>', '<cmd>m .-2<cr>==', {})
-vim.keymap.set('n', '<c-down>', '<cmd>m .+1<cr>==', {})
+vim.keymap.set('x', '<c-up>', lazyredraw(":m '<-2<cr>gv=gv"), {})
+vim.keymap.set('x', '<c-down>', lazyredraw(":m '>+1<cr>gv=gv"), {})
+vim.keymap.set('i', '<c-up>', lazyredraw('<cmd>m .-2<cr><c-o>=='), {})
+vim.keymap.set('i', '<c-down>', lazyredraw('<cmd>m .+1<cr><c-o>=='), {})
+vim.keymap.set('n', '<c-up>', lazyredraw('<cmd>m .-2<cr>=='), {})
+vim.keymap.set('n', '<c-down>', lazyredraw('<cmd>m .+1<cr>=='), {})
 
 -- Improve the <home> key.
 vim.keymap.set({ '', 'i' }, '<home>', '<cmd>normal! ^ze<cr>', {})
