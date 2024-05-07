d30705bb83aa6e95c6f5d17f13f20181de7134b3
Declan Mullen <declanmullen3.14@gmail.com>
2024-05-07 10:29:05 -0700

    Added filetype specific keymaps for printf

--- nvim/dot-config/nvim/lua/configs/misc.lua
+++ nvim/dot-config/nvim/lua/configs/misc.lua
@@ -6,8 +6,20 @@ require('oil').setup({
 
 -- PRINTF:
 
-require('printf').setup({
+local printf = require('printf')
+
+printf.setup({
   print_func = { variable = '__PRETTY_FUNCTION__' }
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'c', 'cpp' },
+  callback = function()
+    vim.keymap.set('n', '<leader>dv', printf.print_var, { desc = 'Debug print variable value.', buffer = true })
+    vim.keymap.set('n', '<leader>dl', printf.print_line, { desc = 'Debug print line number.', buffer = true })
+    vim.keymap.set('n', '<leader>df', printf.print_func, { desc = 'Debug print function name.', buffer = true })
+    vim.keymap.set('n', '<leader>dc', printf.clean, { desc = 'Debug print cleanup.', buffer = true })
+  end
 })
 
 -- WHICH-KEY:
