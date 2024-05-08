26a66faa2985931c761e1b71ecb4d1704c39a484
Declan Mullen <declanmullen3.14@gmail.com>
2024-05-07 22:28:56 -0700

    Removed telescope marks keymap

--- nvim/dot-config/nvim/lua/configs/telescope.lua
+++ nvim/dot-config/nvim/lua/configs/telescope.lua
@@ -15,7 +15,6 @@ vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'List open buffers.'
 vim.keymap.set('n', '<leader>fc', builtin.commands, { desc = 'List user commands.' })
 vim.keymap.set('n', '<leader>f:', builtin.command_history, { desc = 'List command history.' })
 vim.keymap.set('n', '<leader>f/', builtin.search_history, { desc = 'List search history.' })
-vim.keymap.set('n', '<leader>fm', builtin.marks, { desc = 'List vim marks.' })
 vim.keymap.set('n', '<leader>fq', builtin.quickfix, { desc = 'List quickfix list items.' })
 vim.keymap.set('n', '<leader>fl', builtin.loclist, { desc = 'List location list items.' })
 
