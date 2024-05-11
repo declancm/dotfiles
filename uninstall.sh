b2f19d0aa0da6ae338e79f27906f1c387d553090
Declan Mullen <declanmullen3.14@gmail.com>
2024-05-10 18:16:46 -0700

    File remove needs to before stow delete

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,7 @@
+rm ~/.config/alacritty/catppuccin-mocha.toml
+
 stow --dotfiles --delete alacritty/
 stow --dotfiles --delete bash/
 stow --dotfiles --delete fonts/
 stow --dotfiles --delete nvim/
 stow --dotfiles --delete tmux/
-
-rm ~/.config/alacritty/catppuccin-mocha.toml
