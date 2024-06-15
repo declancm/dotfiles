674a50622e2639496dba029094128c71cfd9de2b
Declan Mullen <declanmullen3.14@gmail.com>
2024-06-15 12:26:49 -0700

    Dont ignore any treesitter parsers

--- nvim/dot-config/nvim/lua/configs/treesitter.lua
+++ nvim/dot-config/nvim/lua/configs/treesitter.lua
@@ -3,7 +3,6 @@
 require('nvim-treesitter.configs').setup({
   auto_install = true,
   ensure_installed = { 'comment' },
-  ignore_install = { 'c', 'lua', 'vim', 'vimdoc', 'query' },
   highlight = { enable = true },
   indent = { enable = true },
 })
