c290e73349eb6c738d36b859624785643b95db9d
Declan Mullen <declanmullen3.14@gmail.com>
2024-06-16 17:26:31 -0700

    Use '<bar>'

--- nvim/dot-config/nvim/lua/keymaps.lua
+++ nvim/dot-config/nvim/lua/keymaps.lua
@@ -26,7 +26,7 @@ vim.keymap.set('n', '<leader>ct', '<cmd>TrimTrailingWhitespace<cr>', { desc = 'T
 vim.keymap.set('t', '<esc><esc>', '<c-\\><c-n>', { desc = 'Enter normal mode' })
 
 -- Quitting Neovim.
-vim.keymap.set('n', '<leader>q', '<cmd>wa | qa<cr>', { desc = 'Save all windows and quit' })
+vim.keymap.set('n', '<leader>q', '<cmd>wa<bar>qa<cr>', { desc = 'Save all windows and quit' })
 
 -- Line movement.
 vim.keymap.set('v', '<', lazyredraw('<gv'), { desc = 'Shift lines left' })
