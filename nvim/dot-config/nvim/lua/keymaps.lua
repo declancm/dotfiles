2ee9a199ab0f105f601a256210ca763fc87e97ff
Declan Mullen <declanmullen3.14@gmail.com>
2024-06-16 17:09:24 -0700

    Formatted keymaps

--- nvim/dot-config/nvim/lua/keymaps.lua
+++ nvim/dot-config/nvim/lua/keymaps.lua
@@ -7,14 +7,14 @@ vim.keymap.set({ 'n', 'x' }, '<c-u>', lazyredraw('<c-u>zz'), { desc = 'Scroll wi
 vim.keymap.set({ 'n', 'x' }, '<c-d>', lazyredraw('<c-d>zz'), { desc = 'Scroll window downwards' })
 
 -- Navigating windows.
-vim.keymap.set({'n', 'i'}, '<c-h>', '<cmd>wincmd h<cr>', { desc = 'Move left a window' })
-vim.keymap.set({'n', 'i'}, '<c-j>', '<cmd>wincmd j<cr>', { desc = 'Move down a window' })
-vim.keymap.set({'n', 'i'}, '<c-k>', '<cmd>wincmd k<cr>', { desc = 'Move up a window' })
-vim.keymap.set({'n', 'i'}, '<c-l>', '<cmd>wincmd l<cr>', { desc = 'Move right a window' })
-vim.keymap.set({'n', 'i'}, '<c-left>', '<cmd>wincmd h<cr>', { desc = 'Move left a window' })
-vim.keymap.set({'n', 'i'}, '<c-down>', '<cmd>wincmd j<cr>', { desc = 'Move down a window' })
-vim.keymap.set({'n', 'i'}, '<c-up>', '<cmd>wincmd k<cr>', { desc = 'Move up a window' })
-vim.keymap.set({'n', 'i'}, '<c-right>', '<cmd>wincmd l<cr>', { desc = 'Move right a window' })
+vim.keymap.set({ 'n', 'i' }, '<c-h>', '<cmd>wincmd h<cr>', { desc = 'Move left a window' })
+vim.keymap.set({ 'n', 'i' }, '<c-j>', '<cmd>wincmd j<cr>', { desc = 'Move down a window' })
+vim.keymap.set({ 'n', 'i' }, '<c-k>', '<cmd>wincmd k<cr>', { desc = 'Move up a window' })
+vim.keymap.set({ 'n', 'i' }, '<c-l>', '<cmd>wincmd l<cr>', { desc = 'Move right a window' })
+vim.keymap.set({ 'n', 'i' }, '<c-left>', '<cmd>wincmd h<cr>', { desc = 'Move left a window' })
+vim.keymap.set({ 'n', 'i' }, '<c-down>', '<cmd>wincmd j<cr>', { desc = 'Move down a window' })
+vim.keymap.set({ 'n', 'i' }, '<c-up>', '<cmd>wincmd k<cr>', { desc = 'Move up a window' })
+vim.keymap.set({ 'n', 'i' }, '<c-right>', '<cmd>wincmd l<cr>', { desc = 'Move right a window' })
 
 -- Completion.
 vim.keymap.set('i', '<c-space>', '<c-x><c-o>', { desc = 'Trigger omni completion' })
