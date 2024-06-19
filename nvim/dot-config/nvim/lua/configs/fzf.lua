e2b3188d8cb3c534e3acf0b6e76bce308bc9c1c8
Declan Mullen <declanmullen3.14@gmail.com>
2024-06-19 00:53:56 -0700

    Embrace the default fzf keymaps

--- nvim/dot-config/nvim/lua/configs/fzf.lua
+++ nvim/dot-config/nvim/lua/configs/fzf.lua
@@ -6,13 +6,8 @@ local actions = require('fzf-lua.actions')
 fzf.setup({
   keymap = {
     builtin = {
-      ['<c-f>'] = 'preview-page-down',
-      ['<c-b>'] = 'preview-page-up',
-    },
-    fzf = {
-      ['ctrl-a'] = 'select-all',
-      ['ctrl-u'] = 'half-page-up',
-      ['ctrl-d'] = 'half-page-down',
+      ['<a-f>'] = 'preview-page-down',
+      ['<a-b>'] = 'preview-page-up',
     },
   },
   files = {
