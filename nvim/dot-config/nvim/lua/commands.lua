a899b312d73023d102c21dd4236aed886e0d80dc
Declan Mullen <declanmullen3.14@gmail.com>
2024-07-07 11:56:28 -0700

    Simplified sessions

--- nvim/dot-config/nvim/lua/commands.lua
+++ nvim/dot-config/nvim/lua/commands.lua
@@ -19,50 +19,25 @@ end, { desc = 'Trim trailing whitespace for the current document.' })
 -- SESSIONS:
 
 -- Store sessions like undo files.
--- g:sessiondir - The directory where the sessions will be stored.
 
-local get_session_dir = function()
-  local session_dir = vim.g.sessiondir or (vim.fn.stdpath('state') .. '/session')
-  return vim.fn.fnamemodify(vim.fs.normalize(session_dir), ':p')
-end
+local session_dir = vim.fs.joinpath(vim.fn.stdpath('state'), '/session')
 
-local get_session_path = function(session_dir)
+local get_session_path = function()
   local session_name = vim.fs.normalize(vim.fn.getcwd()):gsub('/', '%%')
   return vim.fs.joinpath(session_dir, session_name)
 end
 
 vim.api.nvim_create_user_command('LoadSession', function()
-  local session_path = 'Session.vim'
-  local session = io.open(session_path, 'rb')
-
-  if not session then
-    session_path = get_session_path(get_session_dir())
-    session = io.open(session_path, 'rb')
-  end
-
-  if session then
-    vim.cmd.wincmd({ 'w', count = 1 })
-    vim.cmd(session:read('*all'))
-    vim.v.this_session = vim.fn.fnamemodify(session_path, ':p')
-    vim.notify('Session loaded: ' .. vim.v.this_session)
+  local session = get_session_path()
+  if vim.fn.filereadable(session) ~= 0 then
+    vim.cmd('wincmd 1w') -- Leave any floating windows
+    vim.cmd('silent! source ' .. vim.fn.fnameescape(session))
   end
 end, { desc = 'Load the session for the current working directory.' })
 
 vim.api.nvim_create_user_command('SaveSession', function()
-  -- Create the session.
-  local session_dir = get_session_dir()
-  if vim.fn.isdirectory(session_dir) == 0 then
-    vim.fn.mkdir(session_dir, 'p')
-  end
-  vim.cmd('mksession!')
-
-  -- Move to the sessions directory.
-  local infile = assert(io.open('Session.vim', 'rb'))
-  local outfile = assert(io.open(get_session_path(session_dir), 'wb'))
-  if outfile:write(infile:read('*all')) then
-    os.remove('Session.vim')
-  end
-  vim.v.this_session = vim.fn.fnamemodify(get_session_path(session_dir), ':p')
+  vim.fn.mkdir(session_dir, 'p')
+  vim.cmd('mksession! ' .. vim.fn.fnameescape(get_session_path()))
   vim.notify('Session saved: ' .. vim.v.this_session)
 end, { desc = 'Save the session for the current working directory.' })
 
