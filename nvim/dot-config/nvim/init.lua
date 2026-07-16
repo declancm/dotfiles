10a441e1ebfaa7873510da052786f56cc90f77a5
Declan Mullen <declanmullen3.14@gmail.com>
2026-07-15 17:22:32 -0700

    Remove command fzf keymaps

--- nvim/dot-config/nvim/init.lua
+++ nvim/dot-config/nvim/init.lua
@@ -40,10 +40,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
   end,
 })
 
-vim.keymap.set('n', '<d-p>', '<cmd>Files<cr>')
-vim.keymap.set('n', '<d-o>', '<cmd>Buffers<cr>')
-vim.keymap.set('n', '<d-s-f>', '<cmd>RG<cr>')
-
 vim.keymap.set('n', '<leader>f', '<cmd>Files<cr>')
 vim.keymap.set('n', '<leader>b', '<cmd>Buffers<cr>')
 vim.keymap.set('n', '<leader>g', '<cmd>RG<cr>')
