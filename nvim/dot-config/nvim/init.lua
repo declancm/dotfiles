9d720a35e26c5f194f366784ccf8b0f0b656e29a
Declan Mullen <declanmullen3.14@gmail.com>
2026-07-16 00:06:47 -0700

    Use stylua formatting

--- nvim/dot-config/nvim/init.lua
+++ nvim/dot-config/nvim/init.lua
@@ -1,4 +1,4 @@
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 
 vim.o.number = true
 vim.o.relativenumber = true
@@ -6,53 +6,58 @@ vim.o.undofile = true
 vim.o.swapfile = false
 
 vim.pack.add({
-  'https://github.com/junegunn/fzf', -- fzf.vim dependency
-  'https://github.com/junegunn/fzf.vim',
-  'https://github.com/neovim/nvim-lspconfig',
-  'https://github.com/nvim-mini/mini.nvim',
-  -- TODO: plugin has been archived so replace once upstreamed
-  'https://github.com/nvim-treesitter/nvim-treesitter',
-  'https://github.com/tpope/vim-sleuth', -- smart indents
+	"https://github.com/junegunn/fzf", -- fzf.vim dependency
+	"https://github.com/junegunn/fzf.vim",
+	"https://github.com/neovim/nvim-lspconfig",
+	"https://github.com/nvim-mini/mini.nvim",
+	-- TODO: plugin has been archived so replace once upstreamed
+	"https://github.com/nvim-treesitter/nvim-treesitter",
+	"https://github.com/tpope/vim-sleuth", -- smart indents
 })
 
-require('mini.diff').setup()
+require("mini.diff").setup()
 
-require('nvim-treesitter').install({
-  'c',
-  'cpp',
-  'python',
-  'starlark',
-  'zig',
+require("nvim-treesitter").install({
+	"c",
+	"cpp",
+	"python",
+	"starlark",
+	"zig",
 })
 
-vim.api.nvim_create_autocmd('FileType', {
-  callback = function() pcall(vim.treesitter.start) end,
+vim.api.nvim_create_autocmd("FileType", {
+	callback = function()
+		pcall(vim.treesitter.start)
+	end,
 })
 
 vim.lsp.enable({
-  'zls', -- zig
+	"stylua", -- lua
+	"zls", -- zig
 })
 
-vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(ev)
-    vim.api.nvim_create_autocmd('BufWritePre', {
-      buffer = ev.buf,
-      callback = function() pcall(vim.lsp.buf.format) end,
-    })
-  end,
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(ev)
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			buffer = ev.buf,
+			callback = function()
+				pcall(vim.lsp.buf.format)
+			end,
+		})
+	end,
 })
 
-vim.keymap.set('n', 'ZB', '<cmd>buffer # | bdelete #<cr>')
+vim.keymap.set("n", "ZB", "<cmd>buffer # | bdelete #<cr>")
 
 vim.api.nvim_create_user_command(
-  'Zoxide',
-  "call fzf#run(fzf#wrap({ 'source': 'zoxide query -l <args>', 'sink': 'cd' }))",
-  { nargs = '*' }
+	"Zoxide",
+	"call fzf#run(fzf#wrap({ 'source': 'zoxide query -l <args>', 'sink': 'cd' }))",
+	{ nargs = "*" }
 )
 
-vim.keymap.set('n', '<leader>f', '<cmd>Files<cr>')
-vim.keymap.set('n', '<leader>b', '<cmd>Buffers<cr>')
-vim.keymap.set('n', '<leader>g', '<cmd>RG<cr>')
-vim.keymap.set('n', '<leader>z', '<cmd>Zoxide<cr>')
+vim.keymap.set("n", "<leader>f", "<cmd>Files<cr>")
+vim.keymap.set("n", "<leader>b", "<cmd>Buffers<cr>")
+vim.keymap.set("n", "<leader>g", "<cmd>RG<cr>")
+vim.keymap.set("n", "<leader>z", "<cmd>Zoxide<cr>")
 
-vim.cmd.colorscheme('retrobox')
+vim.cmd.colorscheme("retrobox")
