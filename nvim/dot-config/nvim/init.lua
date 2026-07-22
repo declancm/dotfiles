a839ed37c5ee48e9735b60ccee8a7efc25ef4096
Declan Mullen <declanmullen3.14@gmail.com>
2026-07-21 18:09:25 -0700

    Set cursorline, add auto-resizing on vim resize and more

--- nvim/dot-config/nvim/init.lua
+++ nvim/dot-config/nvim/init.lua
@@ -1,20 +1,23 @@
 vim.g.mapleader = " "
 
+vim.o.cursorline = true
 vim.o.number = true
 vim.o.relativenumber = true
-vim.o.undofile = true
 vim.o.swapfile = false
+vim.o.undofile = true
 
 vim.pack.add({
 	"https://github.com/junegunn/fzf", -- fzf.vim dependency
 	"https://github.com/junegunn/fzf.vim",
 	"https://github.com/neovim/nvim-lspconfig",
-	"https://github.com/nvim-mini/mini.nvim",
+	"https://github.com/nvim-mini/mini.bufremove",
+	"https://github.com/nvim-mini/mini.diff",
 	-- TODO: plugin has been archived so replace once upstreamed
 	"https://github.com/nvim-treesitter/nvim-treesitter",
 	"https://github.com/tpope/vim-sleuth", -- smart indents
 })
 
+require("mini.bufremove").setup()
 require("mini.diff").setup()
 
 require("nvim-treesitter").install({
@@ -47,7 +50,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 	end,
 })
 
-vim.keymap.set("n", "ZB", "<cmd>b # | bd #<cr>")
+vim.keymap.set("n", "ZB", MiniBufremove.delete)
 
 vim.api.nvim_create_user_command(
 	"Zoxide",
@@ -61,5 +64,9 @@ vim.keymap.set("n", "<leader>F", "<cmd>Files<cr>")
 vim.keymap.set("n", "<leader>g", "<cmd>RG<cr>")
 vim.keymap.set("n", "<leader>h", "<cmd>Helptags<cr>")
 vim.keymap.set("n", "<leader>z", "<cmd>Zoxide<cr>")
+
+vim.api.nvim_create_autocmd("VimResized", { command = "wincmd =" })
+
+vim.keymap.set("x", "<d-c>", [["+y]])
 
 vim.cmd.colorscheme("retrobox")
