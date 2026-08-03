8c4f6cf8bbe1b1f66a26acc4ad5710a5ccf5ff38
Declan Mullen <declanmullen3.14@gmail.com>
2026-08-02 21:15:09 -0700

    Add tmux and ghostty configs

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+stow --dotfiles --adopt ghostty/
 stow --dotfiles --adopt nvim/
+stow --dotfiles --adopt tmux/
 stow --dotfiles --adopt zsh/
--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+stow --dotfiles --delete ghostty/
 stow --dotfiles --delete nvim/
+stow --dotfiles --delete tmux/
 stow --dotfiles --delete zsh/
